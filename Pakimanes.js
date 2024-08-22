f391ad57f36a9820efb4bf234cdc2099c67944d2
Erika Guillén <id.eridian@gmail.com>
2024-08-22 15:07:38 -0600

    Agregue carpeta y cambios menores

--- Pakimanes.js
+++ Pakimanes.js
@@ -1,7 +1,7 @@
-var imagenes = [];//arreglo asociativo (string)
-imagenes["Cauchin"] = "cauchin.png";
-imagenes["Pokacho"] = "pokacho.png";
-imagenes["Tocinauro"] = "tocinauro.png";
+var imagenes = []; //arreglo asociativo (string)
+imagenes["Cauchin"] = "./images/cauchin.png";
+imagenes["Pokacho"] = "./images/pokacho.png";
+imagenes["Tocinauro"] = "./images/tocinauro.png";
 // esto es similar(se ejecuta igual) que lo siguiente en notacion JSON
 // var imagenes = {
 //     cauchin: "cauchin.png",
@@ -17,11 +17,12 @@ imagenes["Tocinauro"] = "tocinauro.png";
 // tocinauro.mostrar();
 // Esta es una forma mas corta de hacer lo de arriba
 var coleccion = [];
-coleccion.push(new Pakiman("Cauchin", 100, 30, "Planta"));//.push agrega un elemento al ultimo lugar del arreglo
+coleccion.push(new Pakiman("Cauchin", 100, 30, "Planta")); //.push agrega un elemento al ultimo lugar del arreglo
 coleccion.push(new Pakiman("Pokacho", 80, 50, "Electrico"));
 coleccion.push(new Pakiman("Tocinauro", 120, 40, "Fuego"));
-for (var p of coleccion) {// para cada variable dentro de coleccion coloquelo dentro de "p"(para cada "p" en coleccion)
-    p.mostrar();
+for (var p of coleccion) {
+  // para cada variable dentro de coleccion coloquelo dentro de "p"(para cada "p" en coleccion)
+  p.mostrar();
 }
 //para utilizar el indice se utiliza "in" para utilizar el objeto de usa "of"
 // for (var p in coleccion) {
